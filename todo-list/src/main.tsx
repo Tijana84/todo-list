d7c4f5f0476e948d2e9c3022069d51eb88176fc1
Tijana Savic <tijanasavic1984@gmail.com>
2023-02-15 17:02:47 +0100

    added pages edit-form

--- todo-list/src/main.tsx
+++ todo-list/src/main.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import {Home} from "./Home";
-import {TodoList} from "./TodoList";
-import {TodoForm} from "./TodoForm";
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import { TodoList } from "./TodoList";
+import { TodoForm } from "./TodoForm";
+import { EditForm } from "./EditForm";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/todo-list" element={<TodoList/>}></Route>
-      <Route path="/todo-form" element={<TodoForm/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/todo-list" element={<TodoList />}></Route>
+        <Route path="/todo-form" element={<TodoForm />}></Route>
+        <Route path="/edit-form" element={<EditForm />}></Route>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
