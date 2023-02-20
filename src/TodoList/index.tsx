b8c96f386a72f0ec23893de0ccac7a7abb4b9d47
Tijana Savic <tijanasavic1984@gmail.com>
2023-02-20 17:23:29 +0100

    changed style for todo-list

--- src/TodoList/index.tsx
+++ src/TodoList/index.tsx
@@ -34,54 +34,54 @@ export const TodoList = () => {
 
   return (
     <div>
-      <div className="flex justify-between p-5 mb-6">
-        <span className="text-4xl">todo</span>
-        <button onClick={handlePlus} className="text-4xl">
+      <div className="flex justify-between p-4 mb-8">
+        <span className="text-4xl text-black">todo</span>
+        <button onClick={handlePlus} className="text-4xl text-black">
           +
         </button>
       </div>
-      <div className="flex px-2 mb-10">
+      <div className="flex items-center px-4 mb-8">
         <div className="flex mr-4">
           <button className="h-6 w-6 rounded-full bg-violet"></button>
-          <a href="work" className="ml-2 text-sm">
+          <a href="work" className="ml-2 text-sm text-black">
             work
           </a>
         </div>
         <div className="flex mr-4">
           <button className="h-6 w-6 rounded-full bg-blue"></button>
-          <a href="work" className="ml-2 text-sm">
+          <a href="work" className="ml-2 text-sm text-black">
             stydy
           </a>
         </div>
         <div className="flex mr-4">
           <button className="h-6 w-6 rounded-full bg-red"></button>
-          <a href="work" className="ml-2 text-sm">
+          <a href="work" className="ml-2 text-sm text-black">
             entertainmet
           </a>
         </div>
         <div className="flex mr-4">
           <button className="h-6 w-6 rounded-full bg-green"></button>
-          <a href="work" className="ml-2 text-sm">
+          <a href="work" className="ml-2 text-sm text-black">
             family
           </a>
         </div>
       </div>
       {tasks.map((data: any, index: number) => {
         const classLabel = twMerge(
-          "text-lg font-semibold p-2",
+          "text-base text-black font-semibold mt-4",
           data.isDone ? "line-through" : ""
         );
         const classDescription = twMerge(
-          "text-sm mb-2 p-2",
+          "text-sm text-black text-justify p-4",
           data.isDone ? "line-through" : ""
         );
         return (
-          <div key={index} className="bg-yellow rounded-lg m-2 relative">
+          <div key={index} className="bg-yellow rounded-lg m-2 mb-6 relative">
             {tasks.map((name: any, index: number) => {})}
-            <div className="flex justify-between items-end" key={index}>
+            <div className="flex justify-between items-end px-4" key={index}>
               <h1 className={classLabel}>{data.title}</h1>
               <button
-                className="p-2 text-grey"
+                className="text-grey text-3xl"
                 onClick={() =>
                   setHidden({
                     ...hidden,
@@ -94,17 +94,17 @@ export const TodoList = () => {
               {hidden[index] ? (
                 <div
                   key={index}
-                  className="flex flex-col items-end p-2 absolute top-0 right-4"
+                  className="flex flex-col items-end p-2 absolute top-8 right-0"
                 >
                   <button
                     onClick={() => handleEdit(index)}
-                    className="bg-white h-10 w-32 rounded-xl mb-1"
+                    className="flex items-center pl-4 bg-white h-12 w-36 rounded-t-lg mb-0.5 text-grey text-sm font-semibold"
                   >
-                    Edit
+                    Edit ...
                   </button>
                   <button
                     onClick={() => handleDelete(index)}
-                    className="bg-white h-10 w-32 rounded-xl"
+                    className=" flex items-center pl-4 bg-white h-12 w-36 rounded-b-lg text-grey text-sm font-semibold"
                   >
                     Delete
                   </button>
@@ -113,14 +113,14 @@ export const TodoList = () => {
             </div>
             <p className={classDescription}>{data.description}</p>
             <div className="flex justify-end p-2">
-              <label htmlFor="" className="mr-1 text-xs text-grey">
+              <label htmlFor="" className="mr-1 text-sm text-black">
                 done
               </label>
               <input
                 checked={data.isDone}
                 name="isDone"
                 type="checkbox"
-                className="text-grey rounded-lg"
+                className="text-black bg-yellow rounded-2xl accent-yellow"
                 onChange={() => handleChecked(index)}
               />
             </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {Todo} from "./Todo";
+import { Todo } from "./Todo";
 import { TodoList } from "./TodoList";
 import { TodoForm } from "./TodoForm";
 import { TodoEdit } from "./TodoEdit";
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Todo/>}></Route>
+        <Route path="/" element={<Todo />}></Route>
         <Route path="/todo-list" element={<TodoList />}></Route>
         <Route path="/todo-form" element={<TodoForm />}></Route>
         <Route path="/todo-edit/:index" element={<TodoEdit />}></Route>
