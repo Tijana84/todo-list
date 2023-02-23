a83034bb69e8d940e7e4f968f251ff78eaca0ca6
Tijana Savic <tijanasavic1984@gmail.com>
2023-02-23 14:11:37 +0100

    fixed todo-edit and changed name for photo

--- src/TodoEdit/index.tsx
+++ src/TodoEdit/index.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import photo3Url from "../images/photo3.png";
+import manUrl from "../images/man.png";
 
 export const TodoEdit = () => {
   const params = useParams();
@@ -35,8 +35,8 @@ export const TodoEdit = () => {
   };
 
   return (
-    <>
-      <div className="flex justify-between items-end md:p-10 p-4 mt-4">
+    <div className="flex flex-col justify-center h-screen max-w-screen-xl md:p-10 p-4">
+      <div className="flex justify-between items-end w-full mb-8">
         <button
           onClick={handleCancel}
           className="bg-white text-black md:text-lg text-base"
@@ -50,7 +50,7 @@ export const TodoEdit = () => {
           Edit
         </button>
       </div>
-      <div className="flex flex-col mt-16 ml-8">
+      <div className="flex flex-col">
         <label
           htmlFor="title"
           className="md:text-xl text-lg text-black font-semibold mb-4"
@@ -62,7 +62,7 @@ export const TodoEdit = () => {
           value={editTitle}
           onChange={(event) => setEditTitle(event.target.value)}
           placeholder="edit a title ..."
-          className="md:text-base text-sm md:w-9/12 md:h-12 w-80 h-10 bg-yellow rounded-md mb-8 p-4 border-none item-center"
+          className="md:text-base text-sm w-full md:h-12 h-10 bg-yellow rounded-md mb-8 p-4 border-none item-center"
         />
         <label
           htmlFor="title"
@@ -73,44 +73,38 @@ export const TodoEdit = () => {
         <textarea
           onChange={(event) => setEditDescription(event.target.value)}
           placeholder="edit a decription ..."
-          className="md:text-base text-sm md:w-9/12 md:h-48 w-80 h-32 bg-yellow rounded-md mb-8 p-4"
+          className="md:text-base text-sm w-full md:h-48 h-32 bg-yellow rounded-md resize-none mb-8 p-4"
           value={editDescription}
         />
       </div>
-      <div className="flex flex-col ml-8">
+      <div className="flex flex-col">
         <h1 className="md:text-xl text-lg text-black font-semibold mb-5">
           Tags
         </h1>
         <div className="md:flex md:mr-4">
-          <div className="flex items-center md:ml-12 ml-4 md:bg-yellow md:h-10 md:w-24 md:rounded-md mb-5">
-            <button className="md:h-7 md:w-7 h-6 w-6 rounded-full bg-violet md:ml-2"></button>
-            <a href="work" className="ml-2 md:text-base  text-sm text-black">
-              work
-            </a>
-          </div>
-          <div className="flex items-center md:ml-12 ml-4 md:bg-yellow md:h-10 md:w-24 md:rounded-md mb-5">
-            <button className="md:h-7 md:w-7 h-6 w-6 rounded-full bg-blue md:ml-2"></button>
-            <a href="work" className="ml-2 text-sm text-black">
-              stydy
-            </a>
-          </div>
-          <div className="flex items-center md:ml-12 ml-4 md:bg-yellow md:h-10 md:w-40 md:rounded-md mb-5">
-            <button className="md:h-7 md:w-7 h-6 w-6 rounded-full bg-red md:ml-2"></button>
-            <a href="work" className="ml-2 md:text-base  text-sm text-black">
+          <button className="flex items-center mb-4">
+            <div className="md:h-7 md:w-7 h-6 w-6 rounded-full bg-violet"></div>
+            <span className="ml-2 md:text-base text-sm text-black">work</span>
+          </button>
+          <button className="flex items-center mb-4 md:ml-4">
+            <div className="md:h-7 md:w-7 h-6 w-6 rounded-full bg-blue"></div>
+            <span className="ml-2 md:text-base text-sm text-black">stydy</span>
+          </button>
+          <button className="flex items-center mb-4 md:ml-4">
+            <div className="md:h-7 md:w-7 h-6 w-6 rounded-full bg-red"></div>
+            <span className="ml-2 md:text-base text-sm text-black">
               entertainmet
-            </a>
-          </div>
-          <div className="flex items-center md:ml-12 ml-4 md:bg-yellow md:h-10 md:w-24 md:rounded-md mb-5">
-            <button className="md:h-7 md:w-7 h-6 w-6 rounded-full bg-green md:ml-2"></button>
-            <a href="work" className="ml-2 md:text-base text-sm text-black">
-              family
-            </a>
-          </div>
+            </span>
+          </button>
+          <button className="flex items-center mb-4 md:ml-4">
+            <div className="md:h-7 md:w-7 h-6 w-6 rounded-full bg-green"></div>
+            <span className="ml-2 md:text-base text-sm text-black">family</span>
+          </button>
         </div>
       </div>
       <div className="md:flex hidden">
-        <img src={photo3Url} className="md:mx-10 md:w-1/5" alt="photo boy" />
+        <img src={manUrl} className="md:w-1/5" alt="photo man" />
       </div>
-    </>
+    </div>
   );
 };
